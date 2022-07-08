94ff5eb17db7c3c93fbd8270e8129f288b011889
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-07-08 14:47:47 +0200

    Set up express server with 2 GET routes (JSON/ text return)

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const express = require('express');
 const app = express();
+
+app.get('/movies', (req, res) => {
+  res.json(topTenMovies);
+})
+
+app.get('/', (req, res) => {
+  res.send(`<p>Welcome to ${req.url}, the future home of the MovieApp!</p>`);
+});
+
+app.listen(8080, () => {
+  console.log('Up and running on port 8080!');
+})

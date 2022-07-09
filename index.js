a9eac19ff365bf73fe27e06d5a38f9be7f7d93c1
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-07-09 09:40:53 +0200

    Add array topTenMovies

--- index.js
+++ index.js
@@ -5,6 +5,59 @@ const morgan = require('morgan');
 const fs = require('fs');
 const path = require('path');
 
+let topTenMovies = [
+  {
+    title: 'Blue Velvet',
+    director: 'David Lynch',
+    year: '1986'
+  },
+  {
+    title: 'Dune',
+    director: 'David Lynch',
+    year: '1984'
+  },
+  {
+    title: 'Twin Peaks: Fire Walk with Me',
+    director: 'David Lynch',
+    year: '1992'
+  },
+  {
+    title: 'Alien',
+    director: 'Ridley Scott',
+    year: '1979'
+  },
+  {
+    title: 'Blade Runner',
+    director: 'Ridley Scott',
+    year: '1982'
+  },
+  {
+    title: 'House of Gucci',
+    director: 'Ridley Scott',
+    year: '2021'
+  },
+  {
+    title: 'Aliens',
+    director: 'James Cameron',
+    year: '1986'
+  },
+  {
+    title: 'The Abyss',
+    director: 'James Cameron',
+    year: '1989'
+  },
+  {
+    title: 'Terminator 2: Judgement Day',
+    director: 'James Cameron',
+    year: '1991'
+  },
+  {
+    title: 'Escape from New York',
+    director: 'James Cameron',
+    year: '1981'
+  }
+]
+
 const app = express();
 //create a write stream in append mode
 //log is added to the log.txt file in the root directory

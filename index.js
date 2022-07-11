0a7eba544e834354ea902ba642c76b257278487d
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-07-11 21:41:17 +0200

    Create POST routing to add new users and PUT routing to update user names by ID

--- index.js
+++ index.js
@@ -124,7 +124,39 @@ app.use(morgan('combined', {stream: accessLogStream}));
 //route requests for static files to the /public folder
 app.use(express.static('public'));
 
-//GET methods returning JSON data from /movies searches
+//use body-parser
+app.use(bodyParser.json());
+
+//POST method to add a new user or update user information (CREATE)
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+
+  if (!newUser.name) {
+    const message = 'You must include your name.';
+    res.status(400).send(message);
+  } else {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);  //status 201 = create
+  }
+});
+
+//PUT method to update user info (UPDATE)
+app.put('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find(user => user.id == id);  //truthy converts string to number
+
+  if (!user) {
+    res.status(400).send(`No user with that ID.`);
+  } else {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  }
+});
+
+//GET methods returning JSON data from /movies searches (CREATE)
 app.get('/movies', (req, res) => {
   res.status(200).json(movies);
 });

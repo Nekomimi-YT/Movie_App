0ec657d3b68f2c8e24bb44c4fb7b8140b7362819
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-07-25 14:12:37 +0200

    Initial commit: Create endpoint for registered user login

---
+++ auth.js
@@ -0,0 +1,36 @@
+const jwtSecret = 'your_jwt_secret'; // Same key used in the JWTStrategy
+
+const jwt = require('jsonwebtoken'),
+  passport = require('passport');
+
+require('./passport'); // The local passport file
+
+
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username, // The username encoded in the JWT
+    expiresIn: '7d', // Specifies that the token will expire in 7 days
+    algorithm: 'HS256' // Algorithm used to “sign” or encode the values of the JWT
+  });
+}
+
+/* POST login. */
+module.exports = (router) => {
+  router.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).json({
+          message: 'Something has gone wrong!',
+          user: user
+        });
+      }
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        let token = generateJWTToken(user.toJSON());
+        return res.json({ user, token }); //user: user, token: token
+      });
+    })(req, res);
+  });
+}

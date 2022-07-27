7a5af74e40c1aa9afc3de8b055aa72b4e910ab50
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-07-27 14:35:04 +0200

    Add code to require and use CORS

--- index.js
+++ index.js
@@ -31,6 +31,21 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//importing CORS
+const cors = require('cors');
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 //importing auth.js, Passport and passport.js
 let auth = require('./auth')(app);
 const passport = require('passport');

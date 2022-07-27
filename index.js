401849204f5e2922d9a5c2c2ad573614f7fa0b16
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-07-27 16:42:16 +0200

    Add code to hash new user passwords when creating a new user

--- index.js
+++ index.js
@@ -53,6 +53,7 @@ require('./passport');
 
 //POST method that adds a new user and returns user data as a JSON object (CREATE)
 app.post('/users', (req, res) => {
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({ Username: req.body.Username })
     .then((user) => {
       if (user) {
@@ -61,7 +62,7 @@ app.post('/users', (req, res) => {
         Users
           .create({
             Username: req.body.Username,
-            Password: req.body.Password,
+            Password: hashedPassword,
             Email: req.body.Email,
             Birthday: req.body.Birthday
           })

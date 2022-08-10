e005b0d55bd08b715abd122902d1765b7698c5b0
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-10 18:00:45 +0200

    Update CORS code

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //importing CORS
-//const cors = require('cors');
-//app.use(cors());
+const cors = require('cors');
+app.use(cors());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")

09c1727eda017af1a6627fc40304d6cea77d004a
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-19 10:13:51 +0200

    Remove console.log's and destructure variables

--- passport.js
+++ passport.js
@@ -1,34 +1,25 @@
 const passport = require('passport'),
   LocalStrategy = require('passport-local').Strategy,
-  Models = require('./models.js'),
+  { User } = require('./models.js'),
   passportJWT = require('passport-jwt');
 
-let Users = Models.User,
-  JWTStrategy = passportJWT.Strategy,
+let JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(new LocalStrategy({
   usernameField: 'Username',
   passwordField: 'Password'
 }, (username, password, callback) => {
-  console.log(`${username}  ${password}`);
-  Users.findOne({ Username: username }, (error, user) => {
+  User.findOne({ Username: username }, (error, user) => {
     if (error) {
-      console.log(error);
       return callback(error);
     }
-
     if (!user) {
-      console.log('incorrect username');
       return callback(null, false, {message: 'Incorrect username.'});
     }
-
     if (!user.validatePassword(password)) {
-      console.log('incorrect password');
       return callback(null, false, {message: 'Incorrect password.'});
     }
-
-    console.log('finished');
     return callback(null, user);
   });
 }));
@@ -37,7 +28,7 @@ passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
   secretOrKey: 'your_jwt_secret'
 }, (jwtPayload, callback) => {
-  return Users.findById(jwtPayload._id)
+  return User.findById(jwtPayload._id)
     .then((user) => {
       return callback(null, user);
     })
